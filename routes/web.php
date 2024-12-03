1e582e05b3a40e14d2225cc8a368509e73f10401
Shefii-02 <shefii.km@gmail.com>
2024-12-03 23:51:43 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 require __DIR__.'/auth.php';
 
 
-require __DIR__.'/route/front.php';
 
 require __DIR__.'/route/admin.php';
 
 require __DIR__.'/route/seller.php';
+require __DIR__.'/route/front.php';
