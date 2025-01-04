a59bed02de8f2c9aba07c28aca5acd3ecd1facd6
Shefii-02 <shefii.km@gmail.com>
2025-01-04 23:51:03 +0530

    round

--- resources/views/front/properties/single.blade.php
+++ resources/views/front/properties/single.blade.php
@@ -114,7 +114,7 @@
                                                     <div class="flex flex-column">
                                                         <h4 class="fw-bold">Carpet Area {{ $property->carpet_area ?? 0 }}
                                                             sqft</h4>
-                                                        <span>{{ shorten_price($property->price / ($property->square > 0 ? $property->square : 1)) }}/sqft</span>
+                                                        <span>{{ shorten_price(round(($property->price / ($property->square > 0 ? $property->square : 1)),2)) }}/sqft</span>
                                                     </div>
                                                 </div>
                                                 <div class="col-lg-3 mb-3">
