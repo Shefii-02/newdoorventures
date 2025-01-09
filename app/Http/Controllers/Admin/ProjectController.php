4aeb9611ffbd25a7ab2c5e1509882aa694a729fa
Shefii-02 <shefii.km@gmail.com>
2025-01-09 20:39:05 +0530

    project filter

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -52,7 +52,7 @@ class ProjectController extends Controller
 
         if($request->has('created_at') && $request->created_at != ''){
             $query->where('created_at', '<=', date('Y-m-d 00:00:00',strtotime($request->created_at)));
-            $query->where('created_at', '<=', date('Y-m-d 23:59:59',strtotime($request->created_at)));
+            $query->where('created_at', '>=', date('Y-m-d 23:59:59',strtotime($request->created_at)));
         }
 
         $projects = $query->paginate(10);
