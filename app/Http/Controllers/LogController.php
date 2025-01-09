e0048a4fd25d95358d0bf77176c06453d99710e9
Shefii-02 <shefii.km@gmail.com>
2025-01-09 20:55:32 +0530

    error log file

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -9,8 +9,11 @@ class LogController extends Controller
 {
     public function logValidationError(Request $request)
     {
+        date_default_timezone_set("Asia/Calcutta");   //India time (GMT+5:30)
+
         // Log the response data from the client
-        Log::error('Validation failed: Property error');
+        Log::notice("message");('Validation failed: Property/Property error');
+        Log::info("message");('Time ' . date('d-m-Y H:i:s'));
         Log::error('Validation Errors: ' . json_encode($request->response));
 
         return response()->json(['message' => 'Logged successfully'], 200);
