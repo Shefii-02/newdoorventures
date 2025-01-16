fb2827dfda04ed17926cd20c245f4d3a07ecef3e
Shefii-02 <shefii.km@gmail.com>
2025-01-16 09:21:35 +0530

    type modify

--- resources/views/front/mobile/home.blade.php
+++ resources/views/front/mobile/home.blade.php
@@ -90,8 +90,7 @@
         </div>
     </div>
     <div class="mobile-type-slider px-3 pt-5">
-        <div class="mobile-type-slider-item"
-            {{-- data-slick='{
+        <div class="mobile-type-slider-item" {{-- data-slick='{
                 "autoplay": true,
                 "autoplaySpeed": 2000,
                 "slidesToShow": 3,
@@ -104,8 +103,7 @@
                     {"breakpoint": 768, "settings": {"slidesToShow": 4}},
                     {"breakpoint": 480, "settings": {"slidesToShow": 4}}
                 ]
-            }' --}}
-            >
+            }' --}}>
             <div class="item">
                 <div class="box">
                     <a href="{{ route('public.properties.sale') }}">
@@ -306,11 +304,30 @@
         $('.mobile-type-slider-item').slick({
             "autoplay": true,
             "autoplaySpeed": 2000,
-            "slidesToShow": 1,
+            "slidesToShow": 3,
             "slidesToScroll": 1,
             "arrows": false,
             "dots": true,
             "infinite": true,
+            "responsive": [{
+                    "breakpoint": 1024,
+                    "settings": {
+                        "slidesToShow": 4
+                    }
+                },
+                {
+                    "breakpoint": 768,
+                    "settings": {
+                        "slidesToShow": 4
+                    }
+                },
+                {
+                    "breakpoint": 480,
+                    "settings": {
+                        "slidesToShow": 4
+                    }
+                }
+            ]
         });
 
 
