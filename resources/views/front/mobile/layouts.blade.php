f7a797e44e09f3ab8fca6d7d14d6805e20a32788
Shefii-02 <shefii.km@gmail.com>
2025-01-17 15:41:32 +0530

    mobile effect

--- resources/views/front/mobile/layouts.blade.php
+++ resources/views/front/mobile/layouts.blade.php
@@ -1348,14 +1348,15 @@
 </head>
 
 <body class="dark:bg-slate-900">
+    <div class="js-container ">
 
-
-    <div class="loading-state">
-        <div class="loading"></div>
+        <div class="loading-state">
+            <div class="loading"></div>
+        </div>
+        <div id="alert-container"></div>
+        @yield('content')
+        @include('front.mobile.bottom-tab')
     </div>
-    <div id="alert-container"></div>
-    @yield('content')
-    @include('front.mobile.bottom-tab')
     <!-- Modal -->
     <div class="modal fade z-999" id="BookingModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -2149,6 +2150,130 @@
             };
         }
     </script>
+
+
+<style>
+    @keyframes confetti-slow {
+        0% {
+            transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
+        }
+
+        100% {
+            transform: translate3d(25px, 105vh, 0) rotateX(360deg) rotateY(180deg);
+        }
+    }
+
+    @keyframes confetti-medium {
+        0% {
+            transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
+        }
+
+        100% {
+            transform: translate3d(100px, 105vh, 0) rotateX(100deg) rotateY(360deg);
+        }
+    }
+
+    @keyframes confetti-fast {
+        0% {
+            transform: translate3d(0, 0, 0) rotateX(0) rotateY(0);
+        }
+
+        100% {
+            transform: translate3d(-50px, 105vh, 0) rotateX(10deg) rotateY(250deg);
+        }
+    }
+
+
+    .confetti-container {
+        perspective: 700px;
+        position: fixed;
+        /* Ensures it stays in the background */
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 0;
+        /* Pushes it behind other elements */
+        pointer-events: none;
+        /* Allows clicks to pass through */
+    }
+
+
+    .confetti {
+        position: absolute;
+        z-index: 1;
+        top: -10px;
+        border-radius: 0%;
+    }
+
+    .confetti--animation-slow {
+        animation: confetti-slow 2.25s linear 1 forwards;
+    }
+
+    .confetti--animation-medium {
+        animation: confetti-medium 1.75s linear 1 forwards;
+    }
+
+    .confetti--animation-fast {
+        animation: confetti-fast 1.25s linear 1 forwards;
+    }
+</style>
+
+<script>
+    const Confettiful = function(el) {
+        this.el = el;
+        this.containerEl = null;
+
+        this.confettiFrequency = 9;
+        this.confettiColors = ['#fce18a', '#ff726d', '#b48def', '#f4306d'];
+        this.confettiAnimations = ['slow', 'medium', 'fast'];
+
+        this._setupElements();
+        this._renderConfetti();
+    };
+
+    Confettiful.prototype._setupElements = function() {
+        const containerEl = document.createElement('div');
+        const elPosition = this.el.style.position;
+
+        if (elPosition !== 'relative' || elPosition !== 'absolute') {
+            this.el.style.position = 'relative';
+        }
+
+        containerEl.classList.add('confetti-container');
+
+        this.el.appendChild(containerEl);
+
+        this.containerEl = containerEl;
+    };
+
+    Confettiful.prototype._renderConfetti = function() {
+        this.confettiInterval = setInterval(() => {
+            const confettiEl = document.createElement('div');
+            const confettiSize = Math.floor(Math.random() * 3) + 7 + 'px';
+            const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors
+                .length)];
+            const confettiLeft = Math.floor(Math.random() * this.el.offsetWidth) + 'px';
+            const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this
+                .confettiAnimations.length)];
+
+            confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
+            confettiEl.style.left = confettiLeft;
+            confettiEl.style.width = confettiSize;
+            confettiEl.style.height = confettiSize;
+            confettiEl.style.backgroundColor = confettiBackground;
+
+            confettiEl.removeTimeout = setTimeout(function() {
+                confettiEl.parentNode.removeChild(confettiEl);
+            }, 3000);
+
+            this.containerEl.appendChild(confettiEl);
+        }, 25);
+    };
+
+    window.confettiful = new Confettiful(document.querySelector('.js-container'));
+</script>
+
     <script>
         (function($) {
             var wa_time_out, wa_time_in;
