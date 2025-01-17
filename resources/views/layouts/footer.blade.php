0702ea6ef11bdacc92e8259e6735fb769da228bf
Shefii-02 <shefii.km@gmail.com>
2025-01-17 14:53:07 +0530

    content change

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -207,7 +207,7 @@
                                 © 2024 New Door Ventures. All right reserved. 
                                 <a class="text-theme mt-2 dark:text-black" href="https://www.newdoorventures.co.in" target="_new">www.newdoorventures.co.in</a>
                             </p>
-                            <p class="mb-0 text-gray-300 fs-6 fw-light">Made with <i class="mdi mdi-heart text-danger"></i> by <a target="_blank"
+                            <p class="mb-0 text-gray-300 fs-6 fw-light">Technology Partner  <i class="mdi mdi-heart text-danger"></i>  <a target="_blank"
                                         href="https://whizcreativetech.com/"><b>Whiz Creative Tech</b></a></p>
                         </div>
 
